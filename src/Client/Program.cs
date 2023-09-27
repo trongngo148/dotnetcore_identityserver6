48272dfb715c5f067ee024a5d2a628c70c78908d
Jonathan Ngo <ngohuutrong.jonathan@gmail.com>
2023-09-27 20:53:21 +0700

    add JWT for authentication

---
+++ src/Client/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using IdentityModel.Client;
+
+Console.WriteLine("Hello, World!");
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+}
+Console.WriteLine(disco.TokenEndpoint);
+
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1",
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.AccessToken);
+
+
+//call api 
+client.SetBearerToken(tokenResponse.AccessToken);
+
+var res = await client.GetAsync("https://localhost:6001/identity");
+
+if (!res.IsSuccessStatusCode)
+{
+    Console.WriteLine(res.StatusCode);
+}
+else
+{
+    var doc = JsonDocument.Parse(await res.Content.ReadAsStringAsync()).RootElement;
+    Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions
+    {
+        WriteIndented = true
+    }));
+}
--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -12,9 +12,22 @@ public static class Config
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
-            { };
+        {
+            new ApiScope(name: "api1", displayName: "MyAPI")
+        };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { };
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = 
+                {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"api1"}
+            }
+        };
 }
